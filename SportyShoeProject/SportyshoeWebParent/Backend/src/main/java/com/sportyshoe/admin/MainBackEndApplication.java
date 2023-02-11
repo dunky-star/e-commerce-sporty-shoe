c95df66fc41c8ecf5dd0229b41af0eff0e266604
dunky-star <dukygeoffrey39@gmail.com>
2023-02-11 19:00:13 +0300

    Add Entity scan in multi module Spring Boot app

--- SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/MainBackEndApplication.java
+++ SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/MainBackEndApplication.java
@@ -2,8 +2,10 @@ package com.sportyshoe.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.sportyshoe.common.entity"})
 public class MainBackEndApplication {
 
     public static void main(String[] args) {
