5a3c09802a9242d0ead2fb54824d08a4137413a6
dunky-star <dukygeoffrey39@gmail.com>
2023-02-11 18:41:47 +0300

    Create Role Repository Unit Test

--- SportyShoeProject/SportyshoeCommon/src/main/java/com/sportyshoe/common/entity/Role.java
+++ SportyShoeProject/SportyshoeCommon/src/main/java/com/sportyshoe/common/entity/Role.java
@@ -1,12 +1,15 @@
 package com.sportyshoe.common.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "roles")
-public class Role extends IdBasedEntity {
+public class Role{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
 
     @Column(length = 40, nullable = false, unique = true)
     private String name;
@@ -46,29 +49,12 @@ public class Role extends IdBasedEntity {
         this.description = description;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+    public Integer getId() {
+        return id;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Role other = (Role) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     @Override
