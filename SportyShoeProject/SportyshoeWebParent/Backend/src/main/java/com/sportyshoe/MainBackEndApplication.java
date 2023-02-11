50c5db6cc201670581b533b30724937b58cb9e08
dunky-star <dukygeoffrey39@gmail.com>
2023-02-11 11:59:16 +0300

    Adding admin panel pages

---
+++ SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/MainBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sportyshoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainBackEndApplication.class, args);
+    }
+
+}
