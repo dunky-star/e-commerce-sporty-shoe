5918440648eb71ff36d56095733e8b70c1912c91
dunky-star <dukygeoffrey39@gmail.com>
2023-02-13 18:39:25 +0300

    Code user authentication - login

--- SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/security/WebSecurityConfig.java
+++ SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/security/WebSecurityConfig.java
@@ -15,9 +15,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().authenticated();
     }
 
 }
