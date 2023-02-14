b3033f283b7962fb91ca97608de2044a41e3d617
dunky-star <dukygeoffrey39@gmail.com>
2023-02-14 11:51:52 +0300

    IdBasedEntity creation

---
+++ SportyShoeProject/SportyshoeCommon/src/main/java/com/sportyshoe/common/entity/IdBasedEntity.java
@@ -0,0 +1,21 @@
+package com.sportyshoe.common.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Id;
+
+@MappedSuperclass
+public abstract class IdBasedEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
