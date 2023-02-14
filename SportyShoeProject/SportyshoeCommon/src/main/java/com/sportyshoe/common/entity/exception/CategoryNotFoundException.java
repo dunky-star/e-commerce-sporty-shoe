8d5e9e85d0bd3fd7df1deac67276e438a1968212
dunky-star <dukygeoffrey39@gmail.com>
2023-02-14 17:24:45 +0300

    Code category management module

---
+++ SportyShoeProject/SportyshoeCommon/src/main/java/com/sportyshoe/common/entity/exception/CategoryNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sportyshoe.common.entity.exception;
+
+public class CategoryNotFoundException extends Exception {
+
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+
+}
