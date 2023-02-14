a50a899103f215c4670883c93d8fc751e93e85b1
dunky-star <dukygeoffrey39@gmail.com>
2023-02-14 14:23:42 +0300

    Fix WebSecurityConfigurerAdapter is deprecated in Spring Boot 2.7.3

--- SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/security/SecurityConfiguration.java
+++ SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/security/SecurityConfiguration.java
@@ -2,19 +2,18 @@ package com.sportyshoe.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -26,21 +25,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authenticationProvider(authenticationProvider());
         http.authorizeRequests()
                 .antMatchers("/states/list_by_country/**").hasAnyAuthority("Admin", "Salesperson")
                 .antMatchers("/users/**", "/settings/**", "/countries/**", "/states/**").hasAuthority("Admin")
@@ -77,13 +80,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .rememberMe()
                 .key("AbcDefgHijKlmnOpqrstuv_1234567890")
                 .tokenValiditySeconds(7 * 24 * 60 * 60);
-        ;
-        http.headers().frameOptions().sameOrigin();
+
+
+              http.headers().frameOptions().sameOrigin();
+
+              return http.build();
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
     }
 
 }
