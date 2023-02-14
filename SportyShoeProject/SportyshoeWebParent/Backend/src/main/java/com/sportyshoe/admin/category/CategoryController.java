5b3b2335b0ba5c9d6747dadd98826e00ba241a27
dunky-star <dukygeoffrey39@gmail.com>
2023-02-14 18:26:53 +0300

    Updated user controller and category controller for deletion operations

--- SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/category/CategoryController.java
+++ SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/category/CategoryController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -130,8 +129,8 @@ public class CategoryController {
                                  RedirectAttributes redirectAttributes) {
         try {
             service.delete(id);
-            String categoryDir = "category-images/" + id;
-            AmazonS3Util.removeFolder(categoryDir);
+            String categoryDir = "../category-images/" + id;
+            FileUploadUtil.removeDir(categoryDir);
 
             redirectAttributes.addFlashAttribute("message",
                     "The category ID " + id + " has been deleted successfully");
