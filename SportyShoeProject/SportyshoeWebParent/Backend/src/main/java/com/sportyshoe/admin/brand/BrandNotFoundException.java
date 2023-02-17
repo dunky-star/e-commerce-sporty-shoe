55098b9aeaf5aa67a66278b74e73f8b29e97e2b3
dunky-star <dukygeoffrey39@gmail.com>
2023-02-17 14:45:05 +0300

    Checking uniqueness of the brand

---
+++ SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/brand/BrandNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sportyshoe.admin.brand;
+
+public class BrandNotFoundException extends Exception {
+
+    public BrandNotFoundException(String message) {
+        super(message);
+    }
+
+}
