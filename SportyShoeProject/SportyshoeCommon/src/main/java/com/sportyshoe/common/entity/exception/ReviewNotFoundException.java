62dc1a39735fa2fd58b74a62d86a67f9ad0f290e
dunky-star <dukygeoffrey39@gmail.com>
2023-02-17 18:50:35 +0300

    Code product entity and repository

---
+++ SportyShoeProject/SportyshoeCommon/src/main/java/com/sportyshoe/common/entity/exception/ReviewNotFoundException.java
@@ -0,0 +1,8 @@
+package com.sportyshoe.common.entity.exception;
+
+public class ReviewNotFoundException extends Exception {
+
+    public ReviewNotFoundException(String message) {
+        super(message);
+    }
+}
