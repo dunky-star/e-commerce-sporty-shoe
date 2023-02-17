23a456fd3d097d3a8a60f4369f7c318434dd730c
dunky-star <dukygeoffrey39@gmail.com>
2023-02-17 09:48:25 +0300

    Check Uniqueness of category name and alias.

--- SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/category/CategoryRestController.java
+++ SportyShoeProject/SportyshoeWebParent/Backend/src/main/java/com/sportyshoe/admin/category/CategoryRestController.java
@@ -1,6 +1,5 @@
-package com.sportyshoe.admin.user.category;
+package com.sportyshoe.admin.category;
 
-import com.sportyshoe.admin.category.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.PostMapping;
