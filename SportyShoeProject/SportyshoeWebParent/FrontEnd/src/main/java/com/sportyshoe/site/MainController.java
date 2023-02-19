4a4b8e348ddc917777de7124303dc3e7e14e0285
dunky-star <dukygeoffrey39@gmail.com>
2023-02-19 12:32:50 +0300

    Code list categories in home page

--- SportyShoeProject/SportyshoeWebParent/FrontEnd/src/main/java/com/sportyshoe/site/MainController.java
+++ SportyShoeProject/SportyshoeWebParent/FrontEnd/src/main/java/com/sportyshoe/site/MainController.java
@@ -3,11 +3,13 @@ package com.sportyshoe.site;
 import com.sportyshoe.common.entity.Category;
 import com.sportyshoe.site.category.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@Controller
 public class MainController {
 
     @Autowired
@@ -21,3 +23,4 @@ public class MainController {
         return "index";
     }
 }
+
