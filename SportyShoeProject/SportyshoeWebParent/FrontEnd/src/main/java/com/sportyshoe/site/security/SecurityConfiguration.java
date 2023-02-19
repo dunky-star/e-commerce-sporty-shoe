fbdc1511dce41359c1a0f3907210321cde74fcf8
dunky-star <dukygeoffrey39@gmail.com>
2023-02-19 11:41:20 +0300

    Code Security Configuration

--- SportyShoeProject/SportyshoeWebParent/FrontEnd/src/main/java/com/sportyshoe/site/security/SecurityConfiguration.java
+++ SportyShoeProject/SportyshoeWebParent/FrontEnd/src/main/java/com/sportyshoe/site/security/SecurityConfiguration.java
@@ -4,23 +4,27 @@ package com.sportyshoe.site.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
-
-        http.headers().frameOptions().sameOrigin();
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults());
         return http.build();
     }
 
     @Bean
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
     }
 
 }
